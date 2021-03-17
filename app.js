e0aaa3d4bed8d8d80f6d4f9f220279249cfe115b
Michał Gleba <GlebaM@users.noreply.github.com>
2021-03-17 01:05:33 +0100

    commit-page3

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const languageTogg = document.querySelector(".switch-lang");
+// FIRST SECTION
+const languageTogg = document.querySelector(".switch-lang-wrap");
 const languagesList = document.querySelector(".hidden-lang")
 const arrows = document.querySelectorAll(".arrows")
 
-const allLanguages = document.querySelectorAll(".hidden-lang a")
-const pickedLanguage = document.querySelector(".picked-language")
+const allLanguages = [...document.querySelectorAll(".hidden-lang a ")];
+const pickedLanguage = document.querySelector(".picked-language");
+
+
 
 
 function activeLang() {
@@ -12,7 +15,41 @@ function activeLang() {
 }
 languageTogg.addEventListener("click", activeLang)
 
-function changeLanguage() {
-
+for (const any of allLanguages) {
+    any.addEventListener('click', () => {
+        // console.log(anyOne.textContent);
+        pickedLanguage.textContent = any.textContent;
+    });
 }
-allLanguages.addEventListener("click", changeLanguage)
+
+
+const imgSwitch = document.querySelector(".img-switch");
+const images = document.querySelectorAll('.img-gal');
+const circles = document.querySelectorAll('.round');
+
+// SECOND SECTION
+let j = 1;
+
+function changeImage() {
+
+    for (const img of images) {
+        img.classList.add("hide");
+
+    }
+    for (const circle of circles) {
+        circle.classList.remove("active");
+
+    }
+    if (j < images.length) {
+        images[j].classList.remove("hide");
+        circles[j].classList.add("active");
+    }
+    j++;
+    if (j >= 4) {
+        j = 0
+    }
+}
+
+imgSwitch.addEventListener('click', changeImage);
+
+// THIRD SECTIO
