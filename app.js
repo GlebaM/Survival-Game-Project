54029d3f38aa7e84992304221fa59c876cc63ca0
Michał Gleba <GlebaM@users.noreply.github.com>
2021-03-23 16:44:50 +0100

    Before subscription form

--- app.js
+++ app.js
@@ -1,105 +1,129 @@
-// FIRST SECTION
-const languageTogg = document.querySelector(".switch-lang-wrap");
-const languagesList = document.querySelector(".hidden-lang")
-const arrows = document.querySelectorAll(".arrows")
+$(document).ready(function() {
 
-const allLanguages = [...document.querySelectorAll(".hidden-lang a ")];
-const pickedLanguage = document.querySelector(".picked-language");
+    // // FIRST SECTION - JS
+    // const languageTogg = document.querySelector(".switch-lang-wrap");
+    // const languagesList = document.querySelector(".hidden-lang")
+    // const arrows = document.querySelectorAll(".arrows")
+    // const allLanguages = [...document.querySelectorAll(".hidden-lang a ")];
+    // const pickedLanguage = document.querySelector(".picked-language");
 
+    // function activeLang() {
+    //     languagesList.classList.toggle("active");
+    //     arrows.forEach(arrow => arrow.classList.toggle("active"));
+    // }
+    // languageTogg.addEventListener("click", activeLang)
 
+    // for (const any of allLanguages) {
+    //     any.addEventListener('click', () => {
+    //         pickedLanguage.textContent = any.textContent;
+    //         languagesList.classList.toggle("active");
+    //     });
+    // }
+    // FIRST SECTION - jQuerry
+    const languagesList = $(".hidden-lang")
 
+    function activeLang() {
+        languagesList.toggleClass("active");
+        $(".arrows").toggleClass("active");
+    }
+    $(".switch-lang-wrap").on("click", activeLang)
 
-function activeLang() {
-    languagesList.classList.toggle("active");
-    arrows.forEach(arrow => arrow.classList.toggle("active"));
-}
-languageTogg.addEventListener("click", activeLang)
-
-for (const any of allLanguages) {
-    any.addEventListener('click', () => {
-        // console.log(anyOne.textContent);
-        pickedLanguage.textContent = any.textContent;
+    $(".hidden-lang>a").on('click', function() {
+        $(".picked-language").html($(this).html());
+        languagesList.toggleClass("active");
     });
-}
 
+    // SECOND SECTION
+    // JS used
+    // const imgSwitch = document.querySelector(".img-switch");
+    // const images = document.querySelector('.img-gal');
+    // const circles = document.querySelector('.round');
 
+    // function changeImage() {
 
+    //     for (const img of images) {
+    //         img.classList.add("hide");
+    //     }
+    //     for (const circle of circles) {
+    //         circle.classList.remove("active");
+    //     }
+    //     if (j < images.length) {
+    //         images[j].classList.remove("hide");
+    //         circles[j].classList.add("active");
+    //     }
+    //     j++;
+    //     if (j >= 4) {
+    //         j = 0
+    //     }
+    // }
+    // imgSwitch.addEventListener('click', changeImage);
 
-// SECOND SECTION
-const imgSwitch = document.querySelector(".img-switch");
-const images = document.querySelectorAll('.img-gal');
-const circles = document.querySelectorAll('.round');
-let j = 1;
+    // jQuerry used
+    const images = $('.img-gal');
+    const circles = $('.round');
+    let j = 1;
 
-function changeImage() {
+    function changeImage() {
+        $('.img-gal').addClass('hide');
+        $('.round').removeClass('active');
 
-    for (const img of images) {
-        img.classList.add("hide");
-
+        if (j < images.length) {
+            images[j].classList.remove("hide");
+            circles[j].classList.add("active");
+            j++
+            if (j >= 4) {
+                j = 0
+            }
+        }
     }
-    for (const circle of circles) {
-        circle.classList.remove("active");
+    $(".img-switch").on('click', changeImage);
 
+    // THIRD SECTION
+
+
+    const $h2features = $(".h2-features");
+    const $featureList = $(".feature-list");
+    const $circleList = $("footer div h2");
+
+
+    for (let i = 0; i <= $h2features.length; i++) {
+
+        $($h2features[i]).on("click", function() {
+            $featureList.addClass("hidden");
+            $circleList.removeClass("white");
+            $featureList[i].classList.remove("hidden");
+            $circleList[i].classList.add("white")
+        })
     }
-    if (j < images.length) {
-        images[j].classList.remove("hide");
-        circles[j].classList.add("active");
+
+    // Section sixth - Not finished
+
+    const newsButton = document.querySelector(".submitBtn");
+    const newsInput = document.querySelector(".news-input");
+    // const newsEmail = document.querySelector("[data-mail]");
+    const newsAlert = document.querySelector(".news-alert");
+    // const newsSpan = document.querySelector(".news-alert>p>span")
+
+    function emailAlert() {
+        //  newsEmail.textContent = newsInput.textContent;
+        if (!newsInput.nodeValue) {
+            newsAlert.classList.add('active');
+            setTimeout(() => {
+                newsAlert.classList.remove('active')
+
+            }, 4000);
+
+        } //  else if (newsInput.includes("@")) {
+        //     newsAlert.classList.add('active');
+        //     newsSpan.innerHTML = `Please include "@" in your e-mail address`;
     }
-    j++;
-    if (j >= 4) {
-        j = 0
-    }
-}
 
-imgSwitch.addEventListener('click', changeImage);
+    newsButton.addEventListener("click", emailAlert);
 
-// THIRD SECTION
+    // $(document).ready(function($) {
+    //     $('*:not(.submitBtn)').on("click", function() {
+    //         $('.news-alert').removeClass('active');
+    //     });
+    // });
 
-// const h2features = [...document.querySelectorAll(".h2-features")];
-// const featureList = [...document.querySelectorAll(".feature-list")];
-
-const $h2features = $(".h2-features");
-const $featureList = $(".feature-list");
-const $circleList = $("footer div h2");
-
-
-for (let i = 0; i <= $h2features.length; i++) {
-    $($h2features[i]).on("click", function() {
-        $featureList.addClass("hidden");
-        $circleList.removeClass("white");
-        $featureList[i].classList.remove("hidden");
-        $circleList[i].classList.add("white")
-    })
-}
-
-// Section sixth - Not finished
-
-const newsButton = document.querySelector(".submitBtn");
-const newsInput = document.querySelector(".news-input");
-const newsEmail = document.querySelector("[data-mail]");
-const newsAlert = document.querySelector(".news-alert");
-const newsSpan = document.querySelector(".news-alert>p>span")
-
-
-
-function emailAlert() {
-    //  newsEmail.textContent = newsInput.textContent;
-    if (!newsInput.nodeValue) {
-        newsAlert.classList.add('active');
-        setTimeout(() => {
-            newsAlert.classList.remove('active')
-
-        }, 4000);
-
-    } //  else if (newsInput.includes("@")) {
-    //     newsAlert.classList.add('active');
-    //     newsSpan.innerHTML = `Please include "@" in your e-mail address`;
-}
-
-newsButton.addEventListener("click", emailAlert);
-
-// $(document).ready(function($) {
-//     $('*:not(.submitBtn)').on("click", function() {
-//         $('.news-alert').removeClass('active');
-//     });
-// });
+});
